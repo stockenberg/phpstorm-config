63e4b6b1f2d84336756ad7e6a15d97ebaa2aaa66
Marten Stockenberg <MStockenberg@googlemail.com>
2017-10-13 12:34:47 +0200

    PS-2017.2.4 <workstation@workstation-ThinkPad-T470 Create baseRefactoring.xml, _@user_Material Theme - Default.icls, ide.general.xml, _@user_Material Theme - Darker.icls, Default for KDE copy.xml, print.xml, project.default.xml, ea_extended.xml, other.xml, juicy.icls, Default.xml, ignore.xml, keymap.xml, code.style.schemes, find.xml, ui.lnf.xml, php.xml, _@user_Material Theme - Lighter.icls, watcherDefaultTasks.xml, notifications.xml, git.xml, databaseDrivers.xml, github_settings.xml, bootstrap.xml, _@user_Material Theme - Palenight.icls, Default.xml, PHP.xml, VueComponent.vue, laf.xml, earthsong-light.icls, snappy-light.icls, userscape.icls, keyboard.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, editor.xml, webServers.xml, debugger.xml, path.macros.xml

---
+++ fileTemplates/VueComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="" id="">
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            
+            }
+        },
+        mounted() {
+            console.log('component ready..');
+        }
+    }
+</script>
+
+<style>
+
+</style>
